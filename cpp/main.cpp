fe0e314735148470f2bd823b28e7a40428f67cec
Nobstar404 <iqbalfadhillah973@gmail.com>
2025-01-27 00:45:04 +0700

    HEAP VS STACK

--- cpp/main.cpp
+++ cpp/main.cpp
@@ -1,20 +1,34 @@
 #include <iostream>
-#include <string>
 
-template<typename T,int N>
-class Array
+struct Vector
 {
-private:
-	T m_Array[N];
-public:
-	int Get() const { return N; }
+	float x, y, z;
+
+	Vector ()
+		: x(10), y(2), z(3) {}
 };
 
 int main()
 {
-	Array<int ,5> array;
-	std::cout << array.Get() << '\n';
+	int value = 5;
+	int array[9];
+	array[0] = 1;
+	array[1] = 2;
+	array[2] = 3;
+	array[3] = 4;
+	array[4] = 5;
+	Vector vector;
 
+	int* hvalue = new int;
+	*hvalue = 9;
+	int* harray = new int[8];
+	harray[0] = 1;
+	harray[1] = 2;
+	harray[2] = 3;
+	harray[3] = 4;
+	harray[4] = 5;
+	Vector* hvector = new Vector();
+	
 	std::cin.get();
 	return 0;
 }
