3c8454f70440fd6f68abee7f975417295f8a156e
Nobstar404 <iqbalfadhillah973@gmail.com>
2025-02-02 00:11:07 +0700

    lambdas

--- cpp/main.cpp
+++ cpp/main.cpp
@@ -1,34 +1,22 @@
 #include <iostream>
+#include <vector>
+#include <functional>
 
-struct Vector
+void ForEach(const std::vector<int>& values, std::function<void(int)>& func)
 {
-	float x, y, z;
-
-	Vector ()
-		: x(10), y(2), z(3) {}
-};
+    for (int value : values)
+        func(value);
+}
 
 int main()
 {
-	int value = 5;
-	int array[9];
-	array[0] = 1;
-	array[1] = 2;
-	array[2] = 3;
-	array[3] = 4;
-	array[4] = 5;
-	Vector vector;
+    std::vector<int> values = { 2,3,1,4,6 };
 
-	int* hvalue = new int;
-	*hvalue = 9;
-	int* harray = new int[8];
-	harray[0] = 1;
-	harray[1] = 2;
-	harray[2] = 3;
-	harray[3] = 4;
-	harray[4] = 5;
-	Vector* hvector = new Vector();
-	
-	std::cin.get();
-	return 0;
+    int a = 5;
+
+    auto lambda = [=](int value) { std::cout << "Value: " << a << '\n'; };
+    ForEach(values, lambda);
+
+    std::cin.get();
+    return 0;
 }
