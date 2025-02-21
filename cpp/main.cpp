6b799d36e6b8d7846d57233b2ac1883ba987c270
Nobstar404 <iqbalfadhillah973@gmail.com>
2025-02-22 01:43:50 +0700

    DYNAMIC_CASTING

--- cpp/main.cpp
+++ cpp/main.cpp
@@ -1,8 +1,30 @@
 #include "pch.h"
 
+class Entity
+{
+public:
+    virtual void PrintName() {};
+};
+
+class Player : public Entity
+{
+
+};
+
+class Enemy : public Entity
+{
+
+};
+
 int main()
 {
-    std::cout << "jokowi dodol\n";
+    Player* player = new Player();
+    Entity* e1 = new Enemy();
+
+    Entity* ActualPlayer = player;
+
+    Player* p1 = dynamic_cast<Player*>(e1);
+    Player* p0 = dynamic_cast<Player*>(ActualPlayer);
 
     std::cin.get();
     return 0;
