2909a29314917122e13a847b6747ae1d740006c0
Nobstar404 <iqbalfadhillah973@gmail.com>
2025-02-23 06:42:33 +0700

    OPTIONAL

--- cpp/main.cpp
+++ cpp/main.cpp
@@ -1,15 +1,33 @@
 #include "pch.h"
 
-std::tuple<std::string, int> CreatePerson()
+std::optional<std::string> ReadFile(const std::string& filepath)
 {
-    return { "jokowi\n", 99 };
+    std::ifstream stream(filepath);
+    if (stream)
+    {
+        std::string result;
+        //read file
+        stream.close();
+        return result;
+    }
+    return {};
 }
 
 int main()
 {
-    // hanya bisa dijalankan dengan versi c++17
-    auto [name, age] = CreatePerson();
-    std::cout << name << age << '\n';
+    std::optional<std::string> data = ReadFile("data.txt");
+
+    std::string value = data.value_or("Not present\n");
+    std::cout << value;
+
+    if (data)
+    {
+        std::cout << "read file succes\n";
+    }
+    else
+    {
+        std::cout << "read file do not succes\n";
+    }
 
     std::cin.get();
     return 0;
