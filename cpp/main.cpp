47bd6661d328074109d508f68d21570711043639
Nobstar404 <iqbalfadhillah973@gmail.com>
2025-03-03 13:48:29 +0700

    ARGUMENT EVALUATON ORDER

--- cpp/main.cpp
+++ cpp/main.cpp
@@ -1,63 +1,15 @@
 #include "pch.h"
 
-struct AllocationMetrics
+void PrintSum(int a, int b)
 {
-    uint32_t TotalAllocated = 0;
-    uint32_t TotalFree = 0;
-
-    uint32_t CurentUsage() { return TotalAllocated - TotalFree; }
-};
-
-AllocationMetrics s_AllocationMetrics;
-
-void* operator new(size_t size)
-{
-    s_AllocationMetrics.TotalAllocated += size;
-
-    return malloc(size);
-}
-
-void operator delete(void* memory, size_t size)
-{
-    s_AllocationMetrics.TotalFree += size;
-
-    free(memory);
-}
-
-static void PrintMemoryUsage()
-{
-    std::cout << "Memory Usage: " << s_AllocationMetrics.CurentUsage() << "bytes\n";
-}
-
-void Print(const std::string& name)
-{
-    std::cout << "[lvalue] " << name;
-}
-
-// INI AKAN TETAP DIPILIH JIKA ADA
-// JIKA TADAK ADA MAKA AKAN KE FUNCTION ATAS
-void Print(std::string&& name)
-{
-    std::cout << "[rvalue] " << name;
+    std::cout << a << " + " << b << " = " << (a + b) << '\n';
 }
 
 int main()
 {
-    {
-        PrintMemoryUsage();
-        std::string firstName = "joko ";
-        PrintMemoryUsage();
-        std::string lastName = "dodol\n";
-        PrintMemoryUsage();
-        std::string fullName = firstName + lastName;
-        PrintMemoryUsage();
-        Print(firstName + lastName);
-        PrintMemoryUsage();
-        Print(fullName);
-        PrintMemoryUsage();
-    }
-    PrintMemoryUsage();
-
+    int value = 0;
+    PrintSum(value++, value++);
+    //setiap versi hasilnya beda
 
     std::cin.get();
     return 0;
