f176534ac7f86845b7ae32905beac26852778c8e
Nobstar404 <iqbalfadhillah973@gmail.com>
2025-03-04 16:30:01 +0700

    MOVE  SEMANTICS

--- cpp/main.cpp
+++ cpp/main.cpp
@@ -1,15 +1,83 @@
 #include "pch.h"
 
-void PrintSum(int a, int b)
+class Strings
 {
-    std::cout << a << " + " << b << " = " << (a + b) << '\n';
-}
+public:
+    Strings() = default;
+
+    Strings(const char* string)
+    {
+        printf("Created\n");
+        m_Size = strlen(string);
+        m_Data = new char[m_Size];
+        memcpy(m_Data, string, m_Size);
+    }
+
+    Strings(const Strings& other)
+    {
+        printf("copy!\n");
+        m_Size = other.m_Size;
+        m_Data = other.m_Data;
+
+        m_Size = 0;
+        m_Data = nullptr;
+    }
+
+    Strings(Strings&& other) noexcept
+    {
+        printf("Moved!\n");
+        m_Size = other.m_Size;
+        m_Data = other.m_Data;
+
+        m_Size = 0;
+        m_Data = nullptr;
+    }
+
+    ~Strings()
+    {
+        printf("Delete\n");
+        delete m_Data;
+    }
+
+    void Print()
+    {
+        for (uint32_t i = 0; i < m_Size; i++)
+        {
+            printf("%C", m_Data[i]);
+        }
+        printf("\n");
+    }
+
+private:
+    char* m_Data;
+    size_t m_Size;
+};
+
+class Entity
+{
+public:
+    Entity(const Strings& name)
+        : m_Name(name)
+    {
+    }
+
+    Entity(Strings&& name)
+        : m_Name((Strings&&)name)
+    {
+    }
+
+    void PrintName()
+    {
+        m_Name.Print();
+    }
+private:
+    Strings m_Name;
+};
 
 int main()
 {
-    int value = 0;
-    PrintSum(value++, value++);
-    //setiap versi hasilnya beda
+    Entity e("jokowi\n");
+    e.PrintName();
 
     std::cin.get();
     return 0;
---
+++ cpp/pch.h
@@ -0,0 +1,26 @@
+#pragma once
+
+#include <iostream>
+#include <sstream>
+#include <algorithm>
+#include <functional>
+#include <fstream>
+#include <chrono>
+#include <thread>
+#include <memory>
+#include <optional>
+#include <utility>
+
+//data structures
+#include <vector>
+#include <string>
+#include <array>
+#include <set>
+#include <map>
+#include <stack>
+#include <deque>
+#include <unordered_set>
+#include <unordered_map>
+
+//windows API
+#include <windows.h>
